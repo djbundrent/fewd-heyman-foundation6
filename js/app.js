2741956b5c0fe079b6c2f9fa3aa4fc5366a9ba1b
Devin Bundrent <devinbundrent@icloud.com>
2017-07-20 23:48:54 -0400

    added support for slick image carousel

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-$(document).foundation()
+$(document).foundation();
+
+$(document).ready(function() {
+    $('.responsive').slick({
+        dots: true,
+        fade: true,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
